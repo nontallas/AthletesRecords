0dffb09a6de8c8277566a840c97a07e14d6e67e9
nontallas <n.o.ntallas@gmail.com>
2021-09-18 12:33:13 +0300

    Define some TODOs

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,4 +1,18 @@
 package controller;
 
-public class Controller {
+public class Controller implements Command{
+
+// TODO
+    /*
+    *
+    * Create case for every different select
+    * Check in every case if there's already an object
+    * Make selects safe against injections && complete them with where
+    * Create UI
+    *
+    */
+    @Override
+    public void execute() {
+
+    }
 }
